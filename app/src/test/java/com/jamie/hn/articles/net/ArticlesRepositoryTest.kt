cb7ed4b8990f2e5611368dcf32fbcc2e27450937
Jamie McDowell <jamie@i6.io>
2020-03-04 00:05:23 +0000

    Added tests for ArticlesRepository

---
+++ app/src/test/java/com/jamie/hn/articles/net/ArticlesRepositoryTest.kt
@@ -0,0 +1,51 @@
+package com.jamie.hn.articles.net
+
+import com.jamie.hn.articles.domain.Article
+import com.jamie.hn.core.net.HackerNewsService
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class ArticlesRepositoryTest {
+    @MockK
+    private lateinit var hnService: HackerNewsService
+
+    private lateinit var articlesRepository: ArticlesRepository
+
+    @BeforeEach
+    fun setup() {
+        MockKAnnotations.init(this)
+        articlesRepository = ArticlesRepository(hnService)
+    }
+
+    @Test
+    fun `when topStories is called then we call topStories on the service`() {
+        coEvery { hnService.topStories() } returns listOf(1, 2, 3)
+
+        runBlocking {
+            articlesRepository.topStories()
+        }
+
+        coVerify { hnService.topStories() }
+    }
+
+    @Test
+    fun `when story is called then we return the article provided by the service `() {
+        val article = Article()
+        lateinit var resultArticle: Article
+
+        coEvery { hnService.getArticle(1) } returns article
+
+        runBlocking {
+            resultArticle = articlesRepository.story(1)
+        }
+
+        coVerify { hnService.getArticle(1) }
+        assertEquals(article, resultArticle)
+    }
+}
