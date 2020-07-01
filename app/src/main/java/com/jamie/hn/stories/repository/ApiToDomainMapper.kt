5adf7ec1c46f3368616bb9470580456695be11fd
Jamie McDowell <jamie@i6.io>
2020-07-01 01:08:55 +0100

    Added a mapper between API model and Domain model, added test for StoryResourceProvider

---
+++ app/src/main/java/com/jamie/hn/stories/repository/ApiToDomainMapper.kt
@@ -0,0 +1,45 @@
+package com.jamie.hn.stories.repository
+
+import com.jamie.hn.comments.domain.model.Comment
+import com.jamie.hn.stories.domain.model.Story
+import com.jamie.hn.stories.repository.model.ApiComment
+import com.jamie.hn.stories.repository.model.ApiStory
+import org.joda.time.DateTime
+
+class ApiToDomainMapper {
+
+    fun toStoryDomainModel(apiStory: ApiStory): Story {
+        return Story(
+            apiStory.author,
+            apiStory.comments?.map { toCommentDomainModel(it) } ?: emptyList(),
+            apiStory.commentCount,
+            apiStory.commentsUrl,
+            apiStory.domain,
+            apiStory.id,
+            apiStory.score,
+            DateTime.parse(apiStory.time),
+            apiStory.title,
+            apiStory.url
+        )
+    }
+
+    private fun toCommentDomainModel(apiComment: ApiComment): Comment {
+        if (apiComment.commentCount == 0) {
+            return Comment(
+                apiComment.author,
+                emptyList(),
+                apiComment.commentCount,
+                apiComment.text,
+                DateTime.parse(apiComment.time)
+            )
+        }
+
+        return Comment(
+            apiComment.author,
+            apiComment.comments?.map { toCommentDomainModel(it) } ?: emptyList(),
+            apiComment.commentCount,
+            apiComment.text,
+            DateTime.parse(apiComment.time)
+        )
+    }
+}
---
+++ app/src/test/java/com/jamie/hn/stories/ui/StoryResourceProviderTest.kt
@@ -0,0 +1,37 @@
+package com.jamie.hn.stories.ui
+
+import android.content.res.Resources
+import com.jamie.hn.core.BaseTest
+import com.jamie.hn.core.InstantExecutorExtension
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.slot
+import io.mockk.verify
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+@ExtendWith(InstantExecutorExtension::class)
+class StoryResourceProviderTest: BaseTest() {
+
+    @MockK
+    private lateinit var resources: Resources
+
+    private lateinit var storyResourceProvider: StoryResourceProvider
+
+    @BeforeEach
+    fun setup() {
+        MockKAnnotations.init(this)
+
+        every { resources.getQuantityString(any(), any(), any()) } returns "quantity"
+        storyResourceProvider = StoryResourceProvider(resources)
+    }
+
+    @Test
+    fun `when comments is called then get quantity string with correct number`() {
+        storyResourceProvider.comments(4)
+
+        verify { resources.getQuantityString(any(), 4, 4) }
+    }
+}
