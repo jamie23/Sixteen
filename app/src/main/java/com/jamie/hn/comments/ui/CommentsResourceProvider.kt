ab3f5bf783fe20931bf6ba0315a7ef216d4240d9
Jamie McDowell <jamie@i6.io>
2020-11-28 17:38:00 +0000

    Now showing hidden parents comment in the hidden amount of comments

--- app/src/main/java/com/jamie/hn/comments/ui/CommentsResourceProvider.kt
+++ app/src/main/java/com/jamie/hn/comments/ui/CommentsResourceProvider.kt
@@ -6,7 +6,7 @@ import com.jamie.hn.R
 class CommentsResourceProvider(
     private val resources: Resources
 ) {
-    fun children() = resources.getString(R.string.comments_children)
+    fun hidden() = resources.getString(R.string.comments_hidden)
     fun article() = resources.getString(R.string.comments_share_article)
     fun comments() = resources.getString(R.string.comments_share_comments)
 }
